857655240c86a875106a5ec5ab6d1947d1aa1b7f
Kayla Smart <kaylasmart.wma@gmail.com>
2020-02-03 16:19:06 -0500

    added connect-mongodb-sessions for memory store

--- app.js
+++ app.js
@@ -84,11 +84,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// app.use(require('express-session')({
-//     secret: "Gleemer Slaps so Dang hard",
-//     resave: false,
-//     saveUninitialized: false
-// }));
+app.use(require('express-session')({
+    secret: "Gleemer Slaps so Dang hard",
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
