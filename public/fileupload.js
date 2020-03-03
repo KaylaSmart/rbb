2c4d257eed1fce9b6df333bab0bc77b2df04f238
Kayla Smart <kaylasmart.wma@gmail.com>
2020-03-03 17:14:16 -0500

    added file pond image uploader

--- public/fileupload.js
+++ public/fileupload.js
@@ -1,17 +1,18 @@
 FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
-    FilePondPluginFileEncode,
+    FilePondPluginFileEncode
 )
 
 FilePond.setOptions({
     stylePanelAspectRatio: 150/100,
-    imageResizeTargetHeight:150,
+    imageResizeTargetHeight:80,
     imageResizeTargetWidth: 100,
-    allowFileEncode:true
+    allowFileEncode:true,
+    getFileEncodeDataURL:
     // server: 'mongodb://heroku_xpflmzb1:ddpf9gd8uskf0qhob8lp97m1o3@ds049467.mlab.com:49467/heroku_xpflmzb1'
 });
 
-
+getFileEncodeDataURL
 
 FilePond.parse(document.body);
